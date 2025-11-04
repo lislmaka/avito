bdd61256ce35387bbf00e981ce3ca55f9efc788e
Andrey <lislmaka@>
2025-11-05 00:47:30 +0300

    branch feature change

--- avito.py
+++ avito.py
@@ -13,7 +13,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def main(site_name=None):
+def sqlite_insert_update(data, file):
+    for field, value in data.items():
+        sqlite_fields = sqlite3_get_fields_names()
+        if field not in sqlite_fields:
+            sqlite3_add_new_field(field)
+    status = sqlite3_add_new_values(data)
+    print(f"File {file}, params ({len(data)}), operation ({status.upper()})")
+
+
+def parse_one(site_name):
+    file = "new.txt"
+    data = None
+    with open(f"{os.environ.get('AVITO_PATH_FILES')}/{file}") as fp:
+        if site_name == "avito":
+            data = parse_avito(fp, file, fnew=True)
+        elif site_name == "cian":
+            data = parse_cian(fp, file, fnew=True)
+        else:
+            pass
+
+        sqlite_insert_update(data, file)
+
+def parse_many():
     for file in get_raw_files():
         with open(f"{os.environ.get('AVITO_PATH_FILES')}/{file}") as fp:
             if file.startswith("avito"):
@@ -22,27 +44,30 @@ def main(site_name=None):
                 data = parse_cian(fp, file, fnew=False)
                 pass
             elif file.startswith("new"):
-                if get_file_size():
-                    if site_name == "avito":
-                        data = parse_avito(fp, file, fnew=True)
-                    elif site_name == "cian":
-                        data = parse_cian(fp, file, fnew=True)
-                    else:
-                        pass
+                continue
+                # if get_file_size():
+                #     if site_name == "avito":
+                #         data = parse_avito(fp, file, fnew=True)
+                #     elif site_name == "cian":
+                #         data = parse_cian(fp, file, fnew=True)
+                #     else:
+                #         pass
             else:
                 print(f"Неправильное название файла - {file}")
                 continue
 
-            for field, value in data.items():
-                sqlite_fields = sqlite3_get_fields_names()
-                if field not in sqlite_fields:
-                    sqlite3_add_new_field(field)
-            status = sqlite3_add_new_values(data)
-            print(f"File {file}, params ({len(data)}), operation ({status.upper()})")
+            sqlite_insert_update(data, file)
+            # for field, value in data.items():
+            #     sqlite_fields = sqlite3_get_fields_names()
+            #     if field not in sqlite_fields:
+            #         sqlite3_add_new_field(field)
+            # status = sqlite3_add_new_values(data)
+            # print(f"File {file}, params ({len(data)}), operation ({status.upper()})")
 
 
 if __name__ == "__main__":
-    site_name = None
     if len(sys.argv) == 2:
         site_name = sys.argv[1]
-    main(site_name)
+        parse_one(site_name)
+    else:
+        parse_many()
